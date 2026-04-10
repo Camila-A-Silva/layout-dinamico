04610e2c16b60908b569bf94619deb427038a127
Camila Silva <camila.a.silva19@aluno.senai.br>
2026-04-10 08:58:06 -0300

    ADD: continuação das funções

--- script.js
+++ script.js
@@ -8,8 +8,37 @@ const body = document.querySelector('body');
 const newsArticles = document.querySelector('.news');
 
 const loadPreferences = () => {
-    
+    // Cor 
+    const savedColor = localStorage.getItem('primaryColor');
+    if(savedColor) {
+        document.documentElement.style.setProperty('--primary-color', savedColor);
+        colorPicker.value = savedColor;
+    }
+
+    // Cor dos Artigos
+    const savedArticleColor = localStorage.getItem('articleColor');
+    if(savedArticleColor) {
+        document.documentElement.style.setProperty('--article-color', savedArticleColor);
+        articleColorPicker.value = savedArticleColor;
+    }
+
+    // Fonte
+    const savedFont = localStorage.getItem('fontFamily');
+    if(savedFont) {
+        document.documentElement.style.setProperty('--font-family', savedFont);
+        fontSelect.value = savedFont;
+    }
+
+    // Tema
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme === 'dark') {
+        body.classList.add('dark');
+        themeToggle.innerHTML = '<span class="material-icons">light_mode</span>';
+    }
 };
+
+
+
 
 colorPicker.addEventListener('input', (e) => {
     const color = e.target.value; // Capturar o movimento do mouse
@@ -27,8 +56,24 @@ articleColorPicker.addEventListener('input', (e) => {
     localStorage.setItem('articleColor', color);
 });
 
-fontSelect.addEventListener('change', (e) => {});
+fontSelect.addEventListener('change', (e) => {
+    const font = e.target.value;
+    document.documentElement.style.setProperty('--font-family', font);
+    localStorage.setItem('fontFamily', font)
+});
 
-themeToggle.addEventListener('click', () => {});
+themeToggle.addEventListener('click', () => {
+    // verifica se existe a classe. Se existe - tira, se não - coloque
+    body.classList.toggle('dark');
+    // Procura pela classe
+    const isDark = body.classList.contains('dark')
+
+    themeToggle.innerHTML = isDark
+        ? '<span class="material-icons">light_mode</span>'
+        : '<span class="material-icons">dark_mode</span>';
+        // if ternario
+    localStorage.setItem('theme', isDark ? 'dark' : 'light');
+});
 
 // Inicializa a aplicação carregando as preferências
+loadPreferences();
