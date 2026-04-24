6fc5758a0fc417c7817319581f5861438b5bab4a
Camila Silva <camila.a.silva19@aluno.senai.br>
2026-04-24 15:27:06 -0300

    sla oq eu fiz

--- spa-fetch-components/test/test.js
+++ spa-fetch-components/test/test.js
@@ -1,11 +1,20 @@
 // Teste com os exeplos da w3 Schools
 
+let result = "Not Active.";
+let isActive = true;
+console.log(result, isActive)
 
+if (isActive = true) {
+    result = "Active!";
+}else{
+    result = "Not Active"
+}
 
-
+console.log(result, isActive)
 
 try{
-    okmygt
+    
+
 }catch(erro){
     let text = `
     <strong>Nome do erro:</strong> ${erro.name} <br/>
@@ -14,4 +23,23 @@ try{
     `;
 
     document.body.innerHTML = text
+};
+
+
+const carregarComponente = async (caminho, container) => {
+    try {
+        const resposta = await fetch(caminho);
+
+        if (!resposta.ok) {
+            throw new Error('Erro ao carregar componente');
+        }
+
+        const html = await resposta.text();
+
+        container.innerHTML = html;
+
+    }catch(erro){
+        console.error(erro);
+
+    }
 };
